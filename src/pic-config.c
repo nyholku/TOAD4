e9d31119fd7665539d206c7e5a922fcc8780bdc6
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2015-03-21 08:12:33 +0200

    Config using SDCC 3.4.0 compatible "#pragma config"

--- src/pic-config.c
+++ src/pic-config.c
@@ -30,6 +30,48 @@
  * OF SUCH DAMAGE.
  */
 
+
+// These oscillator settings are for 4 MHz XTAL
+#pragma	config PLLDIV = 1
+#pragma	config CPUDIV = OSC1_PLL2
+#pragma	config USBDIV = 2
+#pragma	config FOSC = HSPLL_HS
+
+#pragma	config WDT = OFF
+#pragma	config WDTPS = 1
+#pragma	config LPT1OSC = OFF
+#pragma	config CCP2MX = OFF
+#pragma	config PWRT = ON
+#pragma	config BOR = OFF
+#pragma	config BORV = 0
+#pragma	config VREGEN = ON
+#pragma	config FCMEN = OFF
+#pragma	config IESO = OFF
+#pragma	config MCLRE = ON
+#pragma	config PBADEN = OFF
+#pragma	config STVREN = OFF
+#pragma	config LVP = OFF
+#pragma	config ICPRT = OFF
+#pragma	config XINST = OFF
+#pragma	config DEBUG = OFF
+
+#pragma	config CP0 = OFF
+#pragma	config CP1 = OFF
+#pragma	config CP2 = OFF
+#pragma	config CPB = OFF
+#pragma	config CPD = OFF
+#pragma	config WRT0 = OFF
+#pragma	config WRT1 = OFF
+#pragma	config WRT2 = OFF
+#pragma	config WRTB = OFF
+#pragma	config WRTC = OFF
+#pragma	config WRTD = OFF
+#pragma	config EBTR0 = OFF
+#pragma	config EBTR1 = OFF
+#pragma	config EBTR2 = OFF
+#pragma	config EBTRB = OFF
+
+/*
 __code char __at 0x300000 CONFIG1L = 0x20; // USBDIV=1, CPUDIV=00, PLLDIV = 000
 
 __code char __at 0x300001 CONFIG1H = 0x0E; // IESO=0, FCMEN=0, FOSC = 1110
@@ -57,3 +99,4 @@ __code char __at 0x30000B CONFIG6H = 0xff; // No data/boot/table protection
 __code char __at 0x30000C CONFIG7L = 0xff; // No table read protection
 
 __code char __at 0x30000D CONFIG7H = 0xff; // No boot table protection
+*/
